942dc48d992c36cee7cf19319f59b604832db800
resikrys <resicris@gmail.com>
2025-08-12 11:37:44 +0200

    upd Agency class -> implements Observer

--- src/main/java/observer/StockExchangeAgency.java
+++ src/main/java/observer/StockExchangeAgency.java
@@ -1,6 +1,16 @@
 package observer; //Agencia -> observador (rep al notificació)
 
-public class StockExchangeAgency {
+public class StockExchangeAgency implements Observer {
     //Implements Observer -> concrete observer
     //String name + method update()... notification
+    private String agencyName;
+
+    public StockExchangeAgency(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    @Override
+    public void update(String change) {
+        System.out.println("Agency " + agencyName + ": ¡Alert! The Stock Exchange has experienced a chnage: " + change + ".");
+    }
 }
