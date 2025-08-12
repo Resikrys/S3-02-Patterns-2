175c5dc32086c1dd4aae4a894facbc5b276429d4
resikrys <resicris@gmail.com>
2025-08-12 10:20:04 +0200

    upd runDemo with director class

--- src/main/java/builder/RunDemo.java
+++ src/main/java/builder/RunDemo.java
@@ -17,45 +17,77 @@ public class RunDemo {
     public void runExamples() {
         System.out.println("--- Pizza Order System ---");
 
-        makeAndShowPizza("Cheesix Pizza", new CheesixPizzaBuilder());
-        makeAndShowPizza("Veggie Pizza", new VeggiePizzaBuilder());
-        makeAndShowPizza("BBQ Pizza", new BbqPizzaBuilder());
+        System.out.println("\n--- Building Cheesix Pizza (using Director) ---");
+        pizzaMaker.setPizzaBuilder(new CheesixPizzaBuilder());
+        Pizza cheesixPizza = pizzaMaker.makeCheesixPizza();
+        System.out.println("Cheesix Pizza: " + cheesixPizza);
 
-        makeAndShowCustomPizza("My Custom Pizza");
-        makeAndShowHawaiianPizza();
-    }
+        System.out.println("\n--- Building Veggie Pizza (using Director) ---");
+        pizzaMaker.setPizzaBuilder(new VeggiePizzaBuilder());
+        Pizza veggiePizza = pizzaMaker.makeVeggiePizza();
+        System.out.println("Veggie Pizza: " + veggiePizza);
 
-    private void makeAndShowPizza(String pizzaName, PizzaBuilder builder) {
-        System.out.println("\n--- Building " + pizzaName + " ---");
-        pizzaMaker.setPizzaBuilder(builder);
-        Pizza pizza = builder.build();
-        System.out.println(pizzaName + ": " + pizza);
-    }
+        System.out.println("\n--- Building BBQ Pizza (using Director) ---");
+        pizzaMaker.setPizzaBuilder(new BbqPizzaBuilder());
+        Pizza bbqPizza = pizzaMaker.makeBbqPizza();
+        System.out.println("BBQ Pizza: " + bbqPizza);
 
-    private void makeAndShowCustomPizza(String pizzaName) {
-        System.out.println("\n--- Building " + pizzaName + " ---");
-        CustomPizzaBuilder customBuilder = new CustomPizzaBuilder();
-        pizzaMaker.setPizzaBuilder(customBuilder);
-
+        System.out.println("\n--- Building My Custom Pizza (using Director) ---");
+        pizzaMaker.setPizzaBuilder(new CustomPizzaBuilder());
         Set<Toppings> customToppings = new HashSet<>();
         customToppings.add(Toppings.CHICKEN);
         customToppings.add(Toppings.MUSHROOMS);
         customToppings.add(Toppings.OLIVES);
+        Pizza myCustomPizza = pizzaMaker.makeCustomPizza(Size.FAMILIAR, Dough.THICK, customToppings);
+        System.out.println("My Custom Pizza: " + myCustomPizza);
 
-        Pizza customPizza = pizzaMaker.makeCustomPizza(Size.FAMILIAR, Dough.THICK, customToppings);
-        System.out.println(pizzaName + ": " + customPizza);
-    }
-
-    private void makeAndShowHawaiianPizza() {
-        System.out.println("\n--- Building Hawaian Pizza ---");
-        CustomPizzaBuilder hawaiianBuilder = new CustomPizzaBuilder();
-        pizzaMaker.setPizzaBuilder(hawaiianBuilder);
-
+        System.out.println("\n--- Building Hawaian Pizza (using Director) ---");
+        pizzaMaker.setPizzaBuilder(new CustomPizzaBuilder());
         Set<Toppings> hawaiianToppings = new HashSet<>();
         hawaiianToppings.add(Toppings.HAM);
         hawaiianToppings.add(Toppings.PINEAPPLE);
-
         Pizza hawaiianPizza = pizzaMaker.makeCustomPizza(Size.SMALL, Dough.THIN_CRUST, hawaiianToppings);
         System.out.println("Hawaian Pizza: " + hawaiianPizza);
+
+//        makeAndShowPizza("Cheesix Pizza", new CheesixPizzaBuilder());
+//        makeAndShowPizza("Veggie Pizza", new VeggiePizzaBuilder());
+//        makeAndShowPizza("BBQ Pizza", new BbqPizzaBuilder());
+//
+//        makeAndShowCustomPizza("My Custom Pizza");
+//        makeAndShowHawaiianPizza();
     }
+
+//    private void makeAndShowPizza(String pizzaName, PizzaBuilder builder) {
+//        System.out.println("\n--- Building " + pizzaName + " ---");
+//        pizzaMaker.setPizzaBuilder(builder);
+//        Pizza pizza = builder.build();
+//        System.out.println(pizzaName + ": " + pizza);
+//    }
+//
+//    private void makeAndShowCustomPizza(String pizzaName) {
+//        System.out.println("\n--- Building " + pizzaName + " ---");
+//        CustomPizzaBuilder customBuilder = new CustomPizzaBuilder();
+//        pizzaMaker.setPizzaBuilder(customBuilder);
+//
+//        Set<Toppings> customToppings = new HashSet<>();
+//        customToppings.add(Toppings.CHICKEN);
+//        customToppings.add(Toppings.MUSHROOMS);
+//        customToppings.add(Toppings.OLIVES);
+//
+//        Pizza customPizza = pizzaMaker.makeCustomPizza(Size.FAMILIAR, Dough.THICK, customToppings);
+//        System.out.println(pizzaName + ": " + customPizza);
+//    }
+//
+//    private void makeAndShowHawaiianPizza() {
+//        System.out.println("\n--- Building Hawaian Pizza ---");
+//        CustomPizzaBuilder hawaiianBuilder = new CustomPizzaBuilder();
+//        pizzaMaker.setPizzaBuilder(hawaiianBuilder);
+//
+//        Set<Toppings> hawaiianToppings = new HashSet<>();
+//        hawaiianToppings.add(Toppings.HAM);
+//        hawaiianToppings.add(Toppings.PINEAPPLE);
+//
+//        Pizza hawaiianPizza = pizzaMaker.makeCustomPizza(Size.SMALL, Dough.THIN_CRUST, hawaiianToppings);
+//        System.out.println("Hawaian Pizza: " + hawaiianPizza);
+//    }
 }
