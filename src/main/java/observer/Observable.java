86423dfb49ab7de7595e91d9f1dc3173a03f630f
resikrys <resicris@gmail.com>
2025-08-12 10:28:54 +0200

    add Observable interface

---
+++ src/main/java/observer/Observable.java
@@ -0,0 +1,7 @@
+package observer;
+
+public interface Observable {
+    void registerObserver(Observer observer);
+    void deleteObserver(Observer observer);
+    void notifyObservers();
+}
--- src/main/java/observer/Stockbroker.java
+++ src/main/java/observer/Stockbroker.java
@@ -2,7 +2,7 @@ package observer; //Agent de Borsa -> notificador (rep l'evento)
 
 import java.util.List;
 
-public class Stockbroker {
+public class Stockbroker implements Observable  {
     //Implements Observable -> concrete observable
     //list observers, double stockPrice, Str stockState
     //methods: addObserver(Obs obs), deleteObs (Obs obs), notifyAll(), getStockPrice, getStockState, stockExchange(?)
