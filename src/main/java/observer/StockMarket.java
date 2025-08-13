e312bd3806eae6de33c6fa5c33cfea762d0f13a8
resikrys <resicris@gmail.com>
2025-08-13 08:53:12 +0200

    new Observable -> stockMarket implementation

--- src/main/java/observer/StockMarket.java
+++ src/main/java/observer/StockMarket.java
@@ -1,23 +1,40 @@
-package observer; //Borsa -> Obj observat (sujeto -> emisor de evento)
+package observer;
 
-public class StockMarket {
-    private final String name;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockMarket implements Observable {
+    private List<Observer> observers;
     private double stockMarketValue;
 
-    public StockMarket(String name, double stockMarketValue) {
-        this.name = name;
-        this.stockMarketValue = stockMarketValue;
+    public StockMarket(double initialValue) {
+        this.observers = new ArrayList<>();
+        this.stockMarketValue = initialValue;
+        System.out.println("StockMarket initiated with value: " + initialValue);
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
     }
 
-    public double getStockMarketValue() {
-        return stockMarketValue;
+    @Override
+    public void deleteObserver(Observer observer) {
+        observers.remove(observer);
     }
 
-    public void setStockMarketValue(double stockMarketValue) {
-        this.stockMarketValue = stockMarketValue;
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(this.stockMarketValue);
+        }
+    }
+
+    public void setStockMarketValue(double newStockMarketValue) {
+        if (this.stockMarketValue != newStockMarketValue) {
+            System.out.println("--- Stock Market value changes from " + this.stockMarketValue + " to " + newStockMarketValue + " ---");
+            this.stockMarketValue = newStockMarketValue;
+            notifyObservers();
+        }
     }
 }
